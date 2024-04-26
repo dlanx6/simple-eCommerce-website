8163d8b02d4f249125a702e9e40586a54eb711a4
dlanx6 <123456789+ronaldjohn.requioma@gmail.com>
2024-04-26 19:53:59 +0800

    added files for deployment

---
+++ script.js
@@ -0,0 +1,49 @@
+let itemNames = JSON.parse(localStorage.getItem('itemNames')) || [];
+let itemPrices = JSON.parse(localStorage.getItem('itemPrices')) || [];
+
+
+function addItem(itemName, itemPrice) {
+    let itemText = document.getElementById(itemName).textContent;
+    let priceText = document.getElementById(itemPrice).textContent;
+
+    itemNames.push(itemText);
+    itemPrices.push(priceText);
+
+    total += cleanPrice(priceText);
+
+    localStorage.setItem('itemNames', JSON.stringify(itemNames));
+    localStorage.setItem('itemPrices', JSON.stringify(itemPrices));
+}
+
+
+function displayCheckout() {
+    let listItem = document.getElementById("item-list");
+    let listPrice = document.getElementById("price-list");
+
+    itemNames.forEach(item => {
+        let p = document.createElement("p");
+        p.textContent = item;
+        listItem.appendChild(p);
+    });
+
+    itemPrices.forEach(price => {
+        let p = document.createElement("p");
+        p.textContent = price;
+        listPrice.appendChild(p);
+    });
+}
+
+function clearItems() {
+    itemNames = [];
+    itemPrices = [];
+
+    localStorage.removeItem('itemNames');
+    localStorage.removeItem('itemPrices');
+
+    let divItem = document.getElementById("item-list");
+    let divPrice = document.getElementById("price-list");
+    divItem.innerHTML = ""; 
+    divPrice.innerHTML = ""; 
+
+    displayCheckout();
+}
